4f676976fb44cd9ba537d15950a63629d051c5a0
shaggy245 <shaggy245@users.noreply.github.com>
2017-05-09 20:09:09 -0500

    finished day7-2

--- day7/day7-2.py
+++ day7/day7-2.py
@@ -3,14 +3,24 @@
 
 You would also like to know which IPs support SSL (super-secret listening).
 
-An IP supports SSL if it has an Area-Broadcast Accessor, or ABA, anywhere in the supernet sequences (outside any square bracketed sections), and a corresponding Byte Allocation Block, or BAB, anywhere in the hypernet sequences. An ABA is any three-character sequence which consists of the same character twice with a different character between them, such as xyx or aba. A corresponding BAB is the same characters but in reversed positions: yxy and bab, respectively.
+An IP supports SSL if it has an Area-Broadcast Accessor, or ABA, anywhere in
+the supernet sequences (outside any square bracketed sections), and a
+corresponding Byte Allocation Block, or BAB, anywhere in the hypernet sequences.
+An ABA is any three-character sequence which consists of the same character
+twice with a different character between them, such as xyx or aba. A
+corresponding BAB is the same characters but in reversed positions: yxy and
+bab, respectively.
 
 For example:
 
-aba[bab]xyz supports SSL (aba outside square brackets with corresponding bab within square brackets).
+aba[bab]xyz supports SSL (aba outside square brackets with corresponding bab
+within square brackets).
 xyx[xyx]xyx does not support SSL (xyx, but no corresponding yxy).
-aaa[kek]eke supports SSL (eke in supernet with corresponding kek in hypernet; the aaa sequence is not related, because the interior character must be different).
-zazbz[bzb]cdb supports SSL (zaz has no corresponding aza, but zbz has a corresponding bzb, even though zaz and zbz overlap).
+aaa[kek]eke supports SSL (eke in supernet with corresponding kek in hypernet;
+the aaa sequence is not related, because the interior character must be
+different).
+zazbz[bzb]cdb supports SSL (zaz has no corresponding aza, but zbz has a
+corresponding bzb, even though zaz and zbz overlap).
 How many IPs in your puzzle input support SSL?
 """
 
@@ -18,19 +28,29 @@ import argparse
 import re
 
 
-def has_palindrome(ip_list):
+def find_aba(ip_list):
     try:
+        aba_list = []
         for ip in ip_list:
             for i in range(0, (len(ip) - 2)):
                 if ip[i] == ip[i + 2] and ip[i] != ip[i + 1]:
-                    return ip[i:i+2]
-        return False
+                    aba_list.append(ip[i:i+3])
+        return aba_list
     except Exception as exc:
         print(exc)
 
 
+def has_bab(aba_list, bab_check):
+    for aba in aba_list:
+        bab = aba[1] + aba[0] + aba[1]
+        if bab in bab_check:
+            return True
+    return False
+
+
 parser = argparse.ArgumentParser(description='Advent of code.')
-parser.add_argument('inputfile', type=argparse.FileType('r'), help='Path to input file')
+parser.add_argument('inputfile', type=argparse.FileType('r'),
+                    help='Path to input file')
 args = parser.parse_args()
 lines = args.inputfile.read().rstrip("\n").split("\n")
 
@@ -38,12 +58,9 @@ tls_count = 0
 
 for line in lines:
     temp = re.split("\[|\]", line)
-    supernet_tri = has_palindrome([temp[i] for i in range(0, len(temp), 2)])
-    if supernet_tri:
-        has_palindrome([temp[i] for i in range(0, len(temp), 2)])
-
-
-    if (has_palindrome([temp[i] for i in range(0, len(temp), 2)])
-        and not has_palindrome([temp[i] for i in range(1, len(temp), 2)])):
+    aba_list = find_aba([temp[i] for i in range(0, len(temp), 2)])
+    bab_check = "-".join([temp[i] for i in range(1, len(temp), 2)])
+    if aba_list and has_bab(aba_list, bab_check):
         tls_count += 1
+
 print(tls_count)

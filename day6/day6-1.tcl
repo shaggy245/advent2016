3e975e1d954a95342d71f793cf9cbb08deee8797
shaggy245 <shaggy245@users.noreply.github.com>
2017-05-30 23:42:23 -0500

    add comments to day6-1.tcl

--- day6/day6-1.tcl
+++ day6/day6-1.tcl
@@ -13,9 +13,16 @@ for {set index 0} {$index < [llength [lindex $lines2 0]]} {incr index} {
 
 # Find most-occurring character in each column
 foreach col $res {
+  # Iterate through unique characters in each column and count
   foreach uchar [lsort -unique $col] {
+    # Append the character and character count {a 3} to a char_count array
+    # Use lsearch -all to create an array containing indexes of occurrences
+    # and count the length of the lsearch results to get the count of character
+    # occurrences.
     lappend char_count "$uchar [llength [lsearch -all $col $uchar]]"
   }
+  # Sort the char_count list of lists by # of occurrences (-index 1) and return
+  # the character with the highest occurrence number. Append that char to code.
   append code "" [lindex [lindex [lsort -integer -decreasing -index 1 $char_count] 0] 0]
   set char_count ""
 }

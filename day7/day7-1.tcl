376c726bf2eef4a889a6a00f0306a9dba01f178f
shaggy245 <shaggy245@users.noreply.github.com>
2017-06-01 08:26:58 -0500

    working on day7-1.tcl

---
+++ day7/day7-1.tcl
@@ -0,0 +1,35 @@
+set infile [open [lindex $argv 0] r]
+set file [read -nonewline $infile]
+close $infile
+
+set lines [split $file "\n"]
+
+set tls_count 0
+foreach line $lines {
+  set in_hypernet 0
+  set found_tls 0
+  for {set idx 0} {$idx < ([llength $line] - 3)} {incr idx} {
+    if {[string index $line [expr ($idx + 3)]] == "\["} {
+      set in_hypernet 1
+      incr idx 3
+      continue
+    } elseif {[string index $line [expr ($idx + 3)]] == "\]"} {
+      set in_hypernet 0
+      incr idx 3
+      continue
+    }
+    #### PROBLEM IN IF CLAUSE
+    if {[string index $line $idx] != [string index $line [expr ($idx + 1)]] and ([string index $line $idx] == [string index $line [expr ($idx + 3)]] and [string index $line [expr ($idx + 1)]] == [string index $line [expr ($idx + 2)]])} {
+      if {in_hypernet == 0} {
+        set found_tls 1
+      } else {
+        set found_tls 0
+        break
+      }
+    }
+  }
+  if {$found_tls == 1} {
+    incr tls_count
+  }
+}
+puts $tls_count

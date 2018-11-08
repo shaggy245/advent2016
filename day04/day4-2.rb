baae58140f4fe38377eaa26b926a694ee9c24ad7
shaggy245 <shaggy245@users.noreply.github.com>
2018-11-07 22:52:29 -0600

    day4-2 in ruby

---
+++ day04/day4-2.rb
@@ -0,0 +1,35 @@
+input = File.readlines(ARGV.first, chomp: true)
+
+def find_legit(ename)
+  characters = Hash.new(0)
+  ename.chars.each do |char|
+    characters[char] += 1
+  end
+  characters.sort_by {|k, v| [-v, k]}.flatten.keep_if {|x| x.is_a? String}[0,5].join()
+end
+
+def decode(room, sector_id)
+  abc = [*('a'..'z')]
+  roomname = ""
+  room.chars.each do |character|
+    if character == '-'
+      roomname += ' '
+    else
+      roomname += abc[(abc.index(character) + sector_id) % 26]
+    end
+  end
+  return roomname
+end
+
+input.each do |line|
+  code = line.split(/[-\[\]]/)
+  ename = code[0..-3].join()
+  sector_id = code[-2].to_i
+  checksum = code[-1]
+  if find_legit(ename) == checksum
+    if decode(code[0..-3].join('-'), sector_id).include? "north"
+      puts sector_id
+    end
+  end
+end
+

8e538ea13041dcd5efe9983d74a0d0a673d6ba88
shaggy245 <shaggy245@users.noreply.github.com>
2018-11-18 12:00:37 -0600

    print code on one line as it is updated

--- day05/day5-2.rb
+++ day05/day5-2.rb
@@ -2,19 +2,23 @@ require 'digest'
 
 input = File.readlines(ARGV.first, chomp: true)
 
+def overprint(password)
+  print "#{password}\r"
+end
+
 input.each do |line|
   counter = 0
   found = 0
   coded = ''
   password = '________'
-  puts password
+  overprint(password)
   until found == 8 do
     md5 = Digest::MD5.new
     coded = md5.hexdigest line + counter.to_s
     #puts coded
     if coded.start_with?('00000') && ('0'..'7').to_a.include?(coded[5]) && password[coded[5].to_i] == '_'
       password[coded[5].to_i] = coded[6]
-      puts password
+      overprint(password)
       found += 1
     end
     counter += 1
